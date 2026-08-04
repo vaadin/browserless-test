071205b2445a7eb6e1cdf98d0b8a3993a056cc89
Tatu Lund <tatu@vaadin.com>
2026-08-04 16:55:27 +0300

    Add GridProTesterTest for GridPro component

---
+++ junit6/src/test/java/com/vaadin/flow/component/gridpro/GridProTesterTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2000-2026 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.component.gridpro;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import com.vaadin.browserless.BrowserlessTest;
+import com.vaadin.browserless.ViewPackages;
+
+import org.junit.jupiter.api.Test;
+
+@ViewPackages
+class GridProTesterTest extends BrowserlessTest {
+
+    @Test
+    void setCheckboxValue_updatesAllRows() {
+        GridProTester<GridPro<GridProView.Bean>, GridProView.Bean> tester = navigateToTester();
+
+        tester.setValue(0, 0, true);
+        tester.setValue(1, 0, true);
+        tester.setValue(2, 0, true);
+
+        List<GridProView.Bean> items = getItems(tester.getComponent());
+        assertEquals(Boolean.TRUE, items.get(0).getChecked());
+        assertEquals(Boolean.TRUE, items.get(1).getChecked());
+        assertEquals(Boolean.TRUE, items.get(2).getChecked());
+    }
+
+    @Test
+    void setName_updatesAllRows() {
+        GridProTester<GridPro<GridProView.Bean>, GridProView.Bean> tester = navigateToTester();
+
+        tester.setValue(0, 1, "Updated Bean 1");
+        tester.setValue(1, 1, "Updated Bean 2");
+        tester.setValue(2, 1, "Updated Bean 3");
+
+        List<GridProView.Bean> items = getItems(tester.getComponent());
+        assertEquals("Updated Bean 1", items.get(0).getName());
+        assertEquals("Updated Bean 2", items.get(1).getName());
+        assertEquals("Updated Bean 3", items.get(2).getName());
+    }
+
+    @Test
+    void setDescription_updatesAllRows() {
+        GridProTester<GridPro<GridProView.Bean>, GridProView.Bean> tester = navigateToTester();
+
+        tester.setValue(0, 2, "Updated Description 1");
+        tester.setValue(1, 2, "Updated Description 2");
+        tester.setValue(2, 2, "Updated Description 3");
+
+        List<GridProView.Bean> items = getItems(tester.getComponent());
+        assertEquals("Updated Description 1", items.get(0).getDescription());
+        assertEquals("Updated Description 2", items.get(1).getDescription());
+        assertEquals("Updated Description 3", items.get(2).getDescription());
+    }
+
+    @Test
+    void setValue_forNonEditColumn_throwsException() {
+        GridProTester<GridPro<GridProView.Bean>, GridProView.Bean> tester = navigateToTester();
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> tester.setValue(0, 3, "Should fail"));
+
+        assertEquals("Column at index 3 is not an EditColumn",
+                exception.getMessage());
+    }
+
+    @SuppressWarnings("unchecked")
+    private GridProTester<GridPro<GridProView.Bean>, GridProView.Bean> navigateToTester() {
+        navigate(GridProView.class);
+        GridPro<GridProView.Bean> gridPro = find(GridPro.class).id("grid-pro");
+        return new GridProTester<>(gridPro);
+    }
+
+    private List<GridProView.Bean> getItems(GridPro<GridProView.Bean> gridPro) {
+        return gridPro.getGenericDataView().getItems().toList();
+    }
+
+}
